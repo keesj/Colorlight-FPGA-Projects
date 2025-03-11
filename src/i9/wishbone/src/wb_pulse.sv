7e87105a27fba04743328f646d9a55e00dfc33ec
keesj <kees.jongenbuurger@gmail.com>
2025-03-11 07:26:05 +0100

    Simplify pwm a little

--- src/i9/wishbone/src/wb_pulse.sv
+++ src/i9/wishbone/src/wb_pulse.sv
@@ -1,9 +1,37 @@
 module pulse (
     input            clk,
     input            rst,
-    output reg [3:0] gpio
+    output reg [3:0] gpio,
+
+    input wire [31:0] pulse_div_di,
+    output wire [31:0] pulse_div_do,
+    input wire pulse_div_valid,
+    output reg pulse_div_ready
 );
 
+ // The ultrasonic sensor can be driven with 2 MOFSETs that can drive the
+ // speaker in twoo directions. This means that during a cycle the 
+ //
+ //          <--du-->
+ //
+ // First wave
+ // 1:       |------|
+ //          |      |
+ // 0:  -----|      |------|      |-------
+ //                        |      |
+ // -1:                    |------|
+ //  
+ //  Second wave, po
+ // 1:  <- ph ->        |------|
+ //                     |      |
+ // 0:             -----|      |------|      |-------
+ //                                   |      |
+ // -1:                               |------|
+ // du: PWM duty
+ // di: PWM divider
+ // ph: Phase of the second wave compared to the first
+ // po: pollarity 
+ //
   // CPU FREQ
   // 1 Hz is the lowest frequency we can generate
   // 50Khz
@@ -11,58 +39,68 @@ module pulse (
   localparam MAX_DIV = CLK_FREQ / 1 / 2;
   localparam MAX = CLK_FREQ / 50_000 / 2;
 
-  localparam WIDTH = $clog2(MAX_DIV);
+  localparam WIDTH = 32;//$clog2(MAX_DIV);
   //localparam PHASE = 60*MAX/360;
-
 
   reg [WIDTH-1:0] pwm_counter;
   reg [WIDTH-1:0] pwm_div;
+  reg [WIDTH-1:0] pwm_next_div;
   reg [WIDTH-1:0] pwm_duty;
-  reg [WIDTH-1:0] pwm_phase;
   reg [WIDTH-1:0] pwm_dp_counter;
   reg [WIDTH-1:0] pwm_dn_counter;
 
+  assign pulse_div_do =pwm_div;
 
+  // Second wave
+  reg [WIDTH-1:0] pwm_phase;
+  reg  pwm_polatiry;
+  reg [WIDTH-1:0] pwm_dp_counter2;
+  reg [WIDTH-1:0] pwm_dn_counter2;
+
+  // 
   assign gpio[0] = pwm_dp_counter > 0;
   assign gpio[1] = pwm_dn_counter > 0;
   assign gpio[2] = 1'b0;
   assign gpio[3] = 1'b0;
 
-  reg last_flip_flop;
   reg flip_flop;
+
+  wire [WIDTH-1:0] pwm_dp_counter_next = (pwm_dp_counter >0 )? pwm_dp_counter: pwm_dp_counter -1;
+  wire [WIDTH-1:0] pwm_dn_counter_next = (pwm_dn_counter >0 )? pwm_dn_counter: pwm_dn_counter -1;
 
   always @(posedge clk) begin
     pwm_counter <= pwm_counter + 1;
-    last_flip_flop <= flip_flop;
 
-    if (pwm_dp_counter > 0) begin
-      pwm_dp_counter <= pwm_dp_counter - 1;
-    end
+    // registers
+    pulse_div_ready <= 0;
 
-    if (pwm_dn_counter > 0) begin
-      pwm_dn_counter <= pwm_dn_counter - 1;
-    end
+    pwm_dp_counter <= pwm_dp_counter_next;
+    pwm_dn_counter <= pwm_dn_counter_next;
 
     if (rst) begin
       pwm_counter <= 0;
       pwm_div <= 10;
+      pwm_next_div <= 10;
       pwm_duty <= 5;
       flip_flop <= 0;
-      last_flip_flop <= 0;
     end else begin
+
+      if (pulse_div_valid) begin
+        pwm_next_div <= pulse_div_di;
+        pulse_div_ready <= 1;
+      end
+
       if (pwm_counter >= pwm_div) begin
         pwm_counter <= 0;
+        pwm_div <= pwm_next_div;
+        if(flip_flop) begin
+          $display("FLIP");
+          pwm_dp_counter <= pwm_duty;
+        end else begin
+          $display("FLOP");
+          pwm_dn_counter <= pwm_duty;
+        end
         flip_flop   <= ~flip_flop;
-      end
-      //risin edge
-      if (flip_flop & ~last_flip_flop) begin
-        pwm_dp_counter <= pwm_duty;
-        $display("%d FLIP", $time());
-      end
-      //falling edge
-      if (~flip_flop & last_flip_flop) begin
-        pwm_dn_counter <= pwm_duty;
-        $display("%d FLOP", $time());
       end
     end
   end
@@ -87,10 +125,21 @@ module wb_pulse (
     output wire [3:0] gpio
 );
 
+
+  reg [31:0] pwm_div_in;
+  reg pwm_div_valid;
+  wire pwm_div_ready;
+  wire [31:0] pwm_div_out;
+
   pulse pwm (
       .clk (clk),
       .rst (rst),
-      .gpio(gpio)
+      .gpio(gpio),
+
+      .pulse_div_di(pwm_div_in),
+      .pulse_div_do(pwm_div_out),
+      .pulse_div_valid(pwm_div_valid),
+      .pulse_div_ready(pwm_div_ready)
   );
 
   reg [31:0] regs[4];
